027655240da950e10c92b669da1e203f8954628e
Stellar2703 <rajammalshash@gmail.com>
2024-10-07 21:13:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setnewTask] = useState("");
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello World</h1>
+      <div className='top'>
+        <input type="text" placeholder="Enter Task"/>
+        <button>Add Task</button>
+
+      </div>
+      </div>
   );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   const name = <h1>Shashwath</h1>
+//   const age = 20
+//   const idgreen = true  
+//   const names = ["Shashwath", "Shash", "Shashu"]
+
+
+//   const [ages, setCount] = useState(0)
+//   const increaseage = () => {
+//     setCount(ages + 1)
+//     console.log(ages)
+//   }
+
+//   return(
+    
+//     <div classname="App">
+//       {name}
+//       {name}
+//       <User name="Shashwath" age="20"/>
+//       <User name="Shash" age="21"/>
+//       <Job salary={10000} position="Developer" company="Google"/>
+      
+//       {age > 18 ? <h1>Eligible</h1> : <h1>Not Eligible</h1>}
+//       <h1 style={{color : idgreen ? "red" : "green" }}>Shashwath</h1>
+//       <h1>{names[1]}</h1>
+//       <button onClick={increaseage}>Increase Age</button>
+//       <h1>{ages}</h1>
+
+
+//     </div>
+//   )
+// }
+
+//   const User = (props) => {
+//     return(
+//       <div>
+//         <h1>{props.name}</h1>
+//         <h2>{props.age}</h2>
+//       </div>
+//     )
+//   }
+
+//   const Job = (props) => {
+//     return(
+//       <div>
+//         <h1>{props.salary}</h1>
+//         <h2>{props.position}</h2>
+//         <h3>{props.company}</h3>
+//       </div>
+//     )
+//   }
 export default App;
